14cf3c641e7e6e305d02503fc5811df1b0a2e9fd
oxleberry <oxleberry@gmail.com>
2018-05-28 07:32:48 -0700

    connected the database

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,12 +2,39 @@
 $(function(){
     console.log('Sanity Check');
 
+    $.ajax({
+      method: 'GET',
+      url: '/api/designs',
+      success: handleSuccess,
+      error: handleError
+    });
 
-
-
+    $('#testing').on('submit', function(e) {
+      e.preventDefault();
+      $.ajax({
+        method: 'POST',
+        url: '/api/designs',
+        data: $(this).serialize(),
+        success: newDesignSuccess,
+        error: handleError
+      });
+    });
 
 
 
 
 
 }); // end of document.ready
+
+
+function handleSuccess() {
+  console.log("loaded success");
+}
+
+function newDesignSuccess() {
+  console.log("created success");
+}
+
+function handleError(e) {
+  console.log('uh oh');
+}
--- server.js
+++ server.js
@@ -3,28 +3,54 @@
 //  SETUP and CONFIGURATION -----------------------
 
 //require express in our app
-var express = require('express'),
-    bodyParser = require('body-parser');
+const express = require('express');
+const bodyParser = require('body-parser');
 
 // generate a new express app and call it 'app'
-var app = express();
+const app = express();
 
 // serve the public directory as a static file directory
 app.use(express.static('public'));
 
 // // body parser config to accept our datatypes
-// app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 
 // Require the models directory in server.js
-var db = require('./models');
-var controllers = require('./controllers');
+const db = require('./models');
+// const controllers = require('./controllers');
 
 
 //  ROUTES --------------------------
 
+// TEST ROUTES
+// const burgers = [
+//   'Hamburger',
+//   'Cheese Burger',
+//   'Vegetable Burger'
+// ];
+//
+// app.get("/api/burgers", (req, res) => {
+//   //send all the burgers
+//   res.json(burgers);
+// });
+
+//_______________
+// ROUTES FROM URL
+// http://localhost:3000/greetings/sharon
+// app.get("/greetings/:name", (req, res) => {
+//   res.send( "Hello, " + req.params.name );
+// });
+
+// http://localhost:3000/thank?name=sharon
+app.get("/thank", (req, res) => {
+  let name = req.query.name;
+  res.send('Thank you, ' + name + '!');
+});
+
 // define a root route: localhost:3000/
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
+  // res.send('Hello World');
   res.sendFile('views/index.html' , { root : __dirname});
 });
 
@@ -32,6 +58,37 @@ app.get('/', function (req, res) {
 // create a new route for GET /api with callback controllers.api.index
 // app.get('/api', controllers.api.index);
 
+
+// DATABASE
+app.get('/api/designs', function (req, res) {
+  // send all books as JSON response
+  db.Design.find(function(err, designs){
+    if (err) {
+      console.log("index error: " + err);
+      res.sendStatus(500);
+    }
+    res.json(designs);
+  });
+});
+
+// CREATE
+app.post('/api/designs', (req, res) => {
+      // let newDesign = req.body;
+      // newDesign.create( newDesign, (err, newDesignSuccess) => {
+      //     if(err) { return console.log(err) }
+      //     res.json(newDesignSuccess);
+      // });
+      let createDesign = new db.Design ({
+          custom_text: req.body.custom_text,
+          design_title: req.body.design_title,
+          designer_name: req.body.designer_name
+      });
+      // create new book in db
+      createDesign.save( {}, (err, newDesignSuccess) => {
+          if(err) { return console.log(err) }
+          res.json(newDesignSuccess);
+      });
+});
 
 
 //  SERVER --------------------------
