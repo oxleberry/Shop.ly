dbfea0bb1d24077405a0908248373442b2d41563
Mashariki White <masharikiwhite@Masharikis-MacBook-Pro.local>
2018-05-31 09:22:31 -0700

    style show & cart add more seed

--- seed.js
+++ seed.js
@@ -24,21 +24,54 @@ const shirtList = [{
     price: 19.99,
     image: 'precision.jpg',
     size: [0, 1, 3, 4, 2, 4],
-    // size: { xs:0, s:1, m:3, L:4, XL:2, XXL:4 },
     description: 'T-shirt are 100% organic cotton. Ethically sourced.'
 }, {
     name: 'bad',
     price: 18.99,
     image: 'bad.jpg',
     size: [1, 4, 3, 2, 3, 0],
-    // size: { xs:1, s:4, m:3, L:2, XL:3, XXL:0 },
     description: 'T-shirt are 100% organic cotton. Ethically sourced.'
 }, {
     name: 'solve problems',
     price: 19.99,
     image: 'solve.jpg',
     size: [3, 0, 1, 2, 3, 4],
-    // size: { xs:3, s:0, m:1, L:2, XL:3, XXL:4 },
+    description: 'T-shirt are 100% organic cotton. Ethically sourced.'
+}, {
+    name: 'care tacos',
+    price: 19.99,
+    image: 'caretacos.jpg',
+    size: [3, 0, 1, 2, 3, 4],
+    description: 'T-shirt are 100% organic cotton. Ethically sourced.'
+}, {
+    name: 'president history',
+    price: 19.99,
+    image: 'faces.jpg',
+    size: [3, 0, 1, 2, 3, 4],
+    description: 'T-shirt are 100% organic cotton. Ethically sourced.'
+}, {
+    name: 'fiery whitehouse',
+    price: 19.99,
+    image: 'fire.jpg',
+    size: [3, 0, 1, 2, 3, 4],
+    description: 'T-shirt are 100% organic cotton. Ethically sourced.'
+}, {
+    name: 'hindsight 2020',
+    price: 19.99,
+    image: 'hindsight.jpg',
+    size: [3, 0, 1, 2, 3, 4],
+    description: 'T-shirt are 100% organic cotton. Ethically sourced.'
+}, {
+    name: 'tacos in hell',
+    price: 19.99,
+    image: 'tacoshell.jpg',
+    size: [3, 0, 1, 2, 3, 4],
+    description: 'T-shirt are 100% organic cotton. Ethically sourced.'
+}, {
+    name: 'tacos think',
+    price: 19.99,
+    image: 'tacothink.jpg',
+    size: [3, 0, 1, 2, 3, 4],
     description: 'T-shirt are 100% organic cotton. Ethically sourced.'
 }];
 
